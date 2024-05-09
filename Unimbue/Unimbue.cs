f3004c3abd2b7899c42fc6894ca4b0260d6bbe01
sjankowskim <sebastian@jankowski.com>
2024-05-09 17:19:25 -0400

    Upload full project

---
+++ Unimbue/Unimbue.cs
@@ -0,0 +1,24 @@
+﻿using ThunderRoad;
+using HarmonyLib;
+using System;
+
+namespace Unimbue
+{
+    public class Unimbue : ThunderScript
+    {
+        public override void ScriptEnable()
+        {
+            base.ScriptEnable();
+            new Harmony("Transfer").PatchAll();
+        }
+
+        [HarmonyPatch(typeof(Imbue), "Transfer", new Type[] { typeof(SpellCastCharge), typeof(float) })]
+        class TransferPatch
+        {
+            public static bool Prefix(Imbue __instance, SpellCastCharge spellCastBase, float energyTransfered)
+            {
+                return !(__instance.spellCastBase == null && spellCastBase.id.Equals("Unimbue"));
+            }
+        }
+    }
+}
